d02aafdf5f2017f483e57d2eb7833ed9e3872b00
Rettana Muon <rettana.muon@gmail.com>
2022-10-22 18:05:23 +0200

    Move default script to drivers

--- install.sh
+++ install.sh
@@ -6,42 +6,45 @@ if (( $EUID != 0 )); then
 fi
 
 if [ $# -eq 0 ] ; then
-  pacman -S xf86-video-amdgpu --noconfirm
-  rsync -avh ./lib/firmware/* /lib/firmware/
   echo "Installation complete!"
   exit
 fi
+
 case $1 in
+  "drivers")
+    pacman -S xf86-video-amdgpu --noconfirm
+    rsync -avh ./lib/firmware/* /lib/firmware/
+    ;;
   "theme")
-  git clone https://github.com/vinceliuice/WhiteSur-kde.git
-  WhiteSur-kde/install.sh
-  pacman -S kvantum latte-dock --noconfirm
-  ;;
+    git clone https://github.com/vinceliuice/WhiteSur-kde.git
+    WhiteSur-kde/install.sh
+    pacman -S kvantum latte-dock --noconfirm
+    ;;
   "ssh")
-  systemctl enable --now sshd.service
+    systemctl enable --now sshd.service
   ;;
   "kvm")
-  pacman -S virt-manager dnsmasq libvirt qemu edk2-ovmf --noconfirm
-  systemctl enable --now libvirtd
-  virsh net-autostart default
-  virsh net-start default
-  cp ./iommuls /usr/bin/
-  usermod -aG libvirt $(whoami)
-  ;;
+    pacman -S virt-manager dnsmasq libvirt qemu edk2-ovmf --noconfirm
+    systemctl enable --now libvirtd
+    virsh net-autostart default
+    virsh net-start default
+    cp ./iommuls /usr/bin/
+    usermod -aG libvirt $(whoami)
+    ;;
   "wallpaper-engine")
-  wget https://ftp.sh.cvut.cz/arch/community/os/x86_64/luajit-2.1.0.beta3.r449.gdad04f17-1-x86_64.pkg.tar.zst
-  pacman -U luajit-2.1.0.beta3.r449.gdad04f17-1-x86_64.pkg.tar.zst --noconfirm
-  sudo pacman -S --noconfirm extra-cmake-modules plasma-framework gst-libav base-devel mpv python-websockets qt5-declarative qt5-websockets qt5-webchannel vulkan-headers
-  git clone https://github.com/catsout/wallpaper-engine-kde-plugin.git
-  cd wallpaper-engine-kde-plugin
-  git submodule update --init
-  mkdir build && cd build
-  cmake .. -DUSE_PLASMAPKG=ON
-  make
-  make install_pkg
-  sudo make install
-  cd ../..
-  ;;
+    wget https://ftp.sh.cvut.cz/arch/community/os/x86_64/luajit-2.1.0.beta3.r449.gdad04f17-1-x86_64.pkg.tar.zst
+    pacman -U luajit-2.1.0.beta3.r449.gdad04f17-1-x86_64.pkg.tar.zst --noconfirm
+    sudo pacman -S --noconfirm extra-cmake-modules plasma-framework gst-libav base-devel mpv python-websockets qt5-declarative qt5-websockets qt5-webchannel vulkan-headers
+    git clone https://github.com/catsout/wallpaper-engine-kde-plugin.git
+    cd wallpaper-engine-kde-plugin
+    git submodule update --init
+    mkdir build && cd build
+    cmake .. -DUSE_PLASMAPKG=ON
+    make
+    make install_pkg
+    sudo make install
+    cd ../..
+    ;;
 esac
 shift 1
 ./install.sh "$@"
