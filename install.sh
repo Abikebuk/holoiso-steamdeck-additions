94b32d08faea608c0f7a63fd921b3021225c9ff7
Rettana Muon <rettana.muon@gmail.com>
2022-10-22 16:32:10 +0200

    Adds theme & ssh

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
-PACKAGES="xf86-video-amdgpu"
-sudo echo ""
-pacman -S "$PACKAGES" --noconfirm
-rsync -avh ./lib/firmware/* /lib/firmware/
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+if [ $# -eq 0 ] ; then
+  PACKAGES="xf86-video-amdgpu"
+  pacman -S "$PACKAGES" --noconfirm
+  rsync -avh ./lib/firmware/* /lib/firmware/
+  exit
+fi
+case $1 in
+  "theme")
+  PACKAGES="kvantum latte-dock"
+  git clone https://github.com/vinceliuice/WhiteSur-kde.git
+  WhiteSur-kde/install.sh
+  pacman -S "$PACKAGES"
+  ;;
+  "ssh")
+  systemctl enable --now sshd.service
+  ;;
+esac
+shift 1
+./install.sh "$@"
+echo "Installation complete!"
--- readme.md
+++ readme.md
@@ -5,9 +5,12 @@ Adds some fixes for Steam Deck.
 * Adds missing ``xf86-video-amdgpu`` that fixes tearing while scrolling in web browsers.
 * Adds missing drivers that fixes missing audio.
   * The files used are extracted from ``steamdeck-recovery-4.img.bz2``, the official SteamOS image for Steam Deck that can be found here : https://store.steampowered.com/steamos/download/?ver=steamdeck&snr=
+* Installation of WhiteSur theme.
+* Enable ssh on boot
 ## How to use
 ```shell
 git clone git@github.com:Abikebuk/holoiso-steamdeck-additions.git
 cd holoiso-steamdeck-additions
 sudo ./install.sh
 ```
+
