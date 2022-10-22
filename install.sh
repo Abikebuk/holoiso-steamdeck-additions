6a77aa140c29c3f1d7a20082540c4c09b053fe54
Rettana Muon <rettana.muon@gmail.com>
2022-10-22 17:26:14 +0200

    add kvm & wallpaper-engine option

--- install.sh
+++ install.sh
@@ -20,6 +20,27 @@ case $1 in
   "ssh")
   systemctl enable --now sshd.service
   ;;
+  "kvm")
+  pacman -S virt-manager dnsmasq libvirt qemu edk2-ovmf
+  systemctl enable --now libvirtd
+  virsh net-autostart default
+  virsh net-start default
+  cp ./iommuls /usr/bin/
+  ;;
+  "wallpaper-engine")
+  wget https://ftp.sh.cvut.cz/arch/community/os/x86_64/luajit-2.1.0.beta3.r449.gdad04f17-1-x86_64.pkg.tar.zst
+  pacman -U luajit-2.1.0.beta3.r449.gdad04f17-1-x86_64.pkg.tar.zst --noconfirm
+  sudo pacman -S extra-cmake-modules plasma-framework gst-libav base-devel mpv python-websockets qt5-declarative qt5-websockets qt5-webchannel vulkan-headers
+  git clone https://github.com/catsout/wallpaper-engine-kde-plugin.git
+  cd wallpaper-engine-kde-plugin
+  git submodule update --init
+  mkdir build && cd build
+  cmake .. -DUSE_PLASMAPKG=ON
+  make
+  make install_pkg
+  sudo make install
+  cd ../..
+  ;;
 esac
 shift 1
 ./install.sh "$@"
